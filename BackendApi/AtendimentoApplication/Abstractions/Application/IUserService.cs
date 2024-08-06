7836c79506ba2a83de7a3c38fa3729967acff9b1
Arthur <arthurgeromello@hotmail.com>
2024-08-06 19:37:13 -0300

    Add user service

---
+++ BackendApi/AtendimentoApplication/Abstractions/Application/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using AtendimentoDomain.Entities;
+
+namespace AtendimentoApplication.Abstractions.Application
+{
+    public interface IUserService
+    {
+        Task<User> Add(User atendimento, CancellationToken ct);
+        Task<bool> Delete(Guid atendimentoId, CancellationToken ct);
+        Task<User?> GetUserById(Guid userId, CancellationToken ct);
+        Task<User?> Update(User atendimento, CancellationToken ct);
+    }
+}
---
+++ BackendApi/AtendimentoApplication/Services/UserService.cs
@@ -0,0 +1,36 @@
+﻿using AtendimentoApplication.Abstractions.Application;
+using AtendimentoApplication.Abstractions.Repository;
+using AtendimentoDomain.Entities;
+
+namespace AtendimentoApplication.Services
+{
+    public sealed class UserService : IUserService
+    {
+        private readonly IUserRepository _repository;
+
+        public UserService(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<User?> GetUserById(Guid userId, CancellationToken ct)
+        {
+            return await _repository.GetUserById(userId, ct);
+        }
+
+        public async Task<User> Add(User atendimento, CancellationToken ct)
+        {
+            return await _repository.Add(atendimento, ct);
+        }
+
+        public async Task<User?> Update(User atendimento, CancellationToken ct)
+        {
+            return await _repository.Update(atendimento, ct);
+        }
+
+        public async Task<bool> Delete(Guid atendimentoId, CancellationToken ct)
+        {
+            return await _repository.Delete(atendimentoId, ct);
+        }
+    }
+}
