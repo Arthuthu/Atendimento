6a9548d861aaf3c3ec9d4339ec2e6f90de1474bd
Arthur <arthurgeromello@hotmail.com>
2024-09-05 18:59:40 -0300

    Add get user by id, fixed methods class

--- src/FrontBlazor/AtendimentoBlazor/Services/AtendimentoService.cs
+++ src/FrontBlazor/AtendimentoBlazor/Services/AtendimentoService.cs
@@ -1,4 +1,5 @@
 ﻿using AtendimentoBlazor.Entities;
+using Newtonsoft.Json;
 
 namespace AtendimentoBlazor.Services
 {
@@ -15,27 +16,6 @@ namespace AtendimentoBlazor.Services
             _logger = logger;
         }
 
-        public async Task<string?> Add(User user)
-        {
-            using FormUrlEncodedContent data = new(
-            [
-                new KeyValuePair<string, string>("Username", user.Username),
-                new KeyValuePair<string, string>("Password", user.Password),
-                new KeyValuePair<string, string>("Email", user.Email)
-            ]);
 
-            string addEndpoint = _config["apiUrl"] + _config["addUser"];
-            HttpResponseMessage authResult = await _client.PostAsync(addEndpoint, data);
-            string authContent = await authResult.Content.ReadAsStringAsync();
-
-            if (authResult.IsSuccessStatusCode is false)
-            {
-                _logger.LogError("Ocorreu para criar o usuario: {authContent}", authContent);
-
-                return await authResult.Content.ReadAsStringAsync(); ;
-            }
-
-            return null;
-        }
     }
 }
--- src/FrontBlazor/AtendimentoBlazor/Services/UserService.cs
+++ src/FrontBlazor/AtendimentoBlazor/Services/UserService.cs
@@ -1,6 +1,60 @@
-﻿namespace AtendimentoBlazor.Services
+﻿using AtendimentoBlazor.Entities;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AtendimentoBlazor.Services
 {
     public sealed class UserService
     {
+        private readonly HttpClient _client;
+        private readonly IConfiguration _config;
+        private readonly ILogger<UserService> _logger;
+        public UserService(HttpClient client, IConfiguration config, ILogger<UserService> logger)
+        {
+            _client = client;
+            _config = config;
+            _logger = logger;
+        }
+        public async Task<User?> GetById(Guid id)
+        {
+            string buscarPorIdEndpoint = _config["apiUrl"] + _config["getUserById"] + $"/{id}";
+            var authResult = await _client.GetAsync(buscarPorIdEndpoint);
+            var authContent = await authResult.Content.ReadAsStringAsync();
+
+            if (authResult.IsSuccessStatusCode is false)
+            {
+                _logger.LogError("Ocorreu um erro durante o carregamento do usuario por id: {authContent}",
+                    authContent);
+
+                return null;
+            }
+
+            var user = JsonConvert.DeserializeObject<User>(authContent);
+
+            return user;
+        }
+
+        public async Task<string?> Add(User user)
+        {
+            using FormUrlEncodedContent data = new
+            ([
+                new KeyValuePair<string, string>("Username", user.Username),
+                new KeyValuePair<string, string>("Password", user.Password),
+                new KeyValuePair<string, string>("Email", user.Email)
+            ]);
+
+            string addEndpoint = _config["apiUrl"] + _config["addUser"];
+            HttpResponseMessage authResult = await _client.PostAsync(addEndpoint, data);
+            string authContent = await authResult.Content.ReadAsStringAsync();
+
+            if (authResult.IsSuccessStatusCode is false)
+            {
+                _logger.LogError("Ocorreu para criar o usuario: {authContent}", authContent);
+
+                return await authResult.Content.ReadAsStringAsync(); ;
+            }
+
+            return null;
+        }
     }
 }
