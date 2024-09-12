8f15fd897709fefad34c5d761288a43b39e44fc6
Arthur <arthurgeromello@hotmail.com>
2024-09-12 18:36:04 -0300

    Add atendimento methods

--- src/FrontBlazor/AtendimentoBlazor/Services/AtendimentoService.cs
+++ src/FrontBlazor/AtendimentoBlazor/Services/AtendimentoService.cs
@@ -16,6 +16,63 @@ namespace AtendimentoBlazor.Services
             _logger = logger;
         }
 
+        public async Task<Atendimento?> GetById(Guid id)
+        {
+            string buscarPorIdEndpoint = _config["apiUrl"] + _config["getAtendimentoById"] + $"/{id}";
+            var authResult = await _client.GetAsync(buscarPorIdEndpoint);
+            var authContent = await authResult.Content.ReadAsStringAsync();
 
+            if (authResult.IsSuccessStatusCode is false)
+            {
+                _logger.LogError("Ocorreu um erro durante o carregamento do atendimento por id: {authContent}",
+                    authContent);
+
+                return null;
+            }
+
+            Atendimento? atendimento = JsonConvert.DeserializeObject<Atendimento>(authContent);
+
+            return atendimento;
+        }
+
+        public async Task<string?> Add(Atendimento atendimento)
+        {
+            using FormUrlEncodedContent data = new
+            ([
+                new KeyValuePair<string, string>("Codigo", atendimento.Codigo),
+                new KeyValuePair<string, string>("Versao", atendimento.Versao),
+                new KeyValuePair<string, string>("Descricao", atendimento.Descricao)
+            ]);
+
+            string addEndpoint = _config["apiUrl"] + _config["addAtendimento"];
+            HttpResponseMessage authResult = await _client.PostAsync(addEndpoint, data);
+            string authContent = await authResult.Content.ReadAsStringAsync();
+
+            if (authResult.IsSuccessStatusCode is false)
+            {
+                _logger.LogError("Ocorreu para criar o atendimento: {authContent}", authContent);
+
+                return await authResult.Content.ReadAsStringAsync(); ;
+            }
+
+            return null;
+        }
+
+        public async Task<string?> Delete(Guid id)
+        {
+            string deleteEndpoint = _config["apiUrl"] + _config["deletarAtendimento"] + $"/{id}";
+            var authResult = await _client.DeleteAsync(deleteEndpoint);
+            var authContent = await authResult.Content.ReadAsStringAsync();
+
+            if (authResult.IsSuccessStatusCode is false)
+            {
+                _logger.LogError("Ocorreu um erro para deletar o atendimento: {authContent}",
+                    authContent);
+
+                return await authResult.Content.ReadAsStringAsync();
+            }
+
+            return null;
+        }
     }
 }
